6b38c4f08ce00c696384bb27d067d1278894d530
Matthew Hawksby <hawkesbee@yahoo.ca>
2023-05-21 02:07:39 -0700

    new commit

--- svelte_extension/src/routes/+page.svelte
+++ svelte_extension/src/routes/+page.svelte
@@ -1,14 +1,12 @@
 <script lang="ts">
     import Divider from "./Divider.svelte";
 
-
     const apiRoute = "https://watchdog-iota.vercel.app/api/gpt/scam"
     let url: string;
 
     let text = ""
 
     let response = ""
-
 
     async function askGPT(){
         fetch(apiRoute, {
@@ -23,46 +21,77 @@
     }
 </script>
 
-<div id="root" >
+<div class="container">
+    <div class="title">Watchdog</div>
+    <div class="text-line">Enter suspicious text here</div>
+    <textarea class="text-box" rows="4"></textarea>
+    <a href="#" class="button">Submit</a>
+</div>
 
-    <h1>
-        Watch_Dog 
-    </h1>
-    <p>Enter your scam here??</p>
-    <textarea placeholder="woof woof"  bind:value={text}/>
+<!--
+<div class="container">
+    <div class="title">Watchdog</div>
+    <div class="text-line">Enter suspicious text here</div>
+    <textarea class="text-box" placeholder = "woof woof" rows="4" bind:value={text}></textarea>
     <button on:click={askGPT}>GO</button>
 
     {#if response}
-        <Divider/>
-        GPT Response:
-        <p>{response}</p>
+    <Divider/>
+    GPT Response:
+    <p>{response}</p>
 
     {:else}
-        Nothing here yet.
+    Nothing here yet.
 
     {/if}
 </div>
 
+    <title>Watchdog</title>
+-->
 <style>
-    #root {
-        padding: 1rem;
-        font-family: "Courier New", monospace;
+    /* CSS styles */
+    body {
+    font-family: Arial, sans-serif;
     }
 
-    h1 {
-        margin: 0.5rem;
-        font-size: larger;
-        text-shadow: 2px 2px #0000005e;
-
-        background: linear-gradient(to left, black 30%, transparent 100%),
-            url(https://grainy-gradients.vercel.app/noise.svg);
-        filter: contrast(200%) brightness(1000%);
+    .container {
+    background-color: #ffffff;
+    border-radius: 10px;
+    padding: 20px;
+    width: 400px;
+    margin: 0 auto;
     }
 
-    textarea {
-        width: 380px;
-        height: 200px;
+    .title {
+    font-size: 24px;
+    text-align: center;
+    margin-bottom: 20px;
     }
 
-    /*  url(https://grainy-gradients.vercel.app/noise.svg);  */
+    .text-line {
+    font-size: 12px;
+    text-align: center;
+    margin-bottom: 10px;
+    }
+
+    .text-box {
+    width: 100%;
+    padding: 10px;
+    border: 1px solid #ccc;
+    border-radius: 5px;
+    margin-bottom: 20px;
+    }
+
+    .button {
+    display: block;
+    width: 120px;
+    height: 40px;
+    background-color: #007bff;
+    color: #fff;
+    text-align: center;
+    line-height: 40px;
+    text-decoration: none;
+    border-radius: 5px;
+    margin-left: auto;
+    }
 </style>
